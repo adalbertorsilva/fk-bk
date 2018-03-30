3dd77c7745cd72e34b4af099cbef3f6e974d4676
Weverson Nascimento <weverson.nascimento@dextra-sw.com>
2018-03-30 20:39:39 -0300

    add cors rules

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const projects = require('./projects/routes');
+const cors = require('koa2-cors');
 
 
 const app = new Koa();
 
+app.use(cors());
 app.use(bodyParser());
 app.use(projects.routes());
 
