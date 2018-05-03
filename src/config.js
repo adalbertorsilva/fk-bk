73d7b573d4aba91de7b7cb751c445eb4c03d508c
Weverson Nascimento <weverson.nascimento@dextra-sw.com>
2018-05-03 16:10:13 -0300

    added docker config in project

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 const config = {
     develop: {
         port: 3000,
-        database: 'mongodb://localhost:27017/mystique-develop',
+        database: 'mongodb://mongo:27017/mystique-develop',
         jwtSecret: 'da39a3ee5e6b4b0d3255bfef95601890afd80709',
     },
     test: {
         port: 3000,
-        database: 'mongodb://localhost:27017/mystique-test',
+        database: 'mongodb://mongo:27017/mystique-test',
         jwtSecret: 'da39a3ee5e6b4b0d3255bfef95601890afd80709',
     },
     production: {
