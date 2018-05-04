85f4c6679bfe9c8303f3c6d5fb3b450409dcd850
Weverson Nascimento <weverson.nascimento@dextra-sw.com>
2018-05-04 15:22:20 -0300

    change mongo uri

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ const config = {
     },
     test: {
         port: 3000,
-        database: 'mongodb://mongo:27017/mystique-test',
+        database: 'mongodb://localhost:27017/mystique-test',
         jwtSecret: 'da39a3ee5e6b4b0d3255bfef95601890afd80709',
     },
     production: {
